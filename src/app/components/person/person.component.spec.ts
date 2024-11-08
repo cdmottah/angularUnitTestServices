5af788ea6cdc9ad981444207731381953555d717
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-07 22:24:48 -0500

    feat(person): update PersonComponent to display dynamic person data
    Modify PersonComponent to display dynamic data using an @Input() person object. Update template to display person's name and height. Adjust unit tests to validate dynamic content rendering for name and height values.

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonComponent } from './person.component';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from '@models/person.model';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -25,18 +26,36 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <p> with "Soy un parrafo"', () => {
+  it('should the name be "Cristian"', () => {
+    component.person = new Person('Cristian', 'Motta', 29, 89, 1.75)
+    expect(component.person.name).toEqual("Cristian")
+  });
+
+  it('should have <p> to contain "1.75"', () => {
+    component.person = new Person('', '', 0, 0, 1.75);
     const personDebug = fixture.debugElement;
     const paragraphDebug: DebugElement = personDebug.query(By.css('p'))
     const paragraphElement = paragraphDebug.nativeElement as HTMLParagraphElement;
-    expect(paragraphElement?.textContent).toEqual("Soy un parrafo")
+
+    fixture.detectChanges();
+
+    expect(paragraphElement?.textContent).toContain("1.75")
   });
 
-  it('should have <h3> with "Hola, personComponent"', () => {
+  it('should have <h3> to contain "Andres"', () => {
+    //Arrange
+    component.person = new Person('Andres', 'Torres', 29, 89, 1.75)
+
     const personDebug = fixture.debugElement;
     const headingDebug: DebugElement = personDebug.query(By.css('h3'))
     const headingElement = headingDebug.nativeElement as HTMLHeadingElement
-    expect(headingElement?.textContent).toEqual("Hola, personComponent")
+    //Act
+    fixture.detectChanges();
+
+    //Assert
+    expect(headingElement?.textContent).toContain("Andres")
   });
+
+
 
 });
