a6f893835dedc9fbae99816ca15c0c018b465e4a
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-07 21:29:41 -0500

    feat(routing): add PersonComponent route and navigation link
    Add a new route for PersonComponent in the app routing module and update the navigation links in the main template. Import and declare PersonComponent in the app module. Create basic structure for PersonComponent with template, styles, and a test spec.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
+import { PersonComponent } from './components/person/person.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { ProductsComponent } from './components/products/products.component';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // {path:'/home',component:HomeComponent},
   { path: 'products', component: ProductsComponent },
-  { path: 'pico-preview', component: PicoPreviewComponent }
+  { path: 'pico-preview', component: PicoPreviewComponent },
+  { path: 'person', component: PersonComponent }
 ];
 
 @NgModule({
