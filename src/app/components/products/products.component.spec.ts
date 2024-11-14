29795cc2e33e6ec375eab73605cf0fb6e4518576
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-13 20:37:05 -0500

    test(products): enhance ProductsComponent tests for product rendering
    Improved ProductsComponent tests by adding cases to verify correct product list display. Moved productMocks to a constant for cleaner setup and reusability. Added a test for getAll in ProductsService to confirm it returns the expected products list. Introduced a test to check proper rendering of product images within the component template.
    
    These changes enhance test accuracy and coverage, ensuring product data is fetched and displayed as expected.

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -4,11 +4,13 @@ import { ProductComponent } from '../product/product.component';
 import { ProductsService } from '@services/products.service';
 import { generateManyProducts } from '@models/product.mock';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
   let productsServiceSpy: jasmine.SpyObj<ProductsService>
+  const productMocks = generateManyProducts(3);
 
   beforeEach(async () => {
     const _productsServiceSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
@@ -25,7 +27,6 @@ fdescribe('ProductsComponent', () => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     productsServiceSpy = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>
-    const productMocks = generateManyProducts(3);
     productsServiceSpy.getAll.and.returnValue(of(productMocks))
     fixture.detectChanges(); // Aquí ya ejecutó el ngOnInit
   });
@@ -33,9 +34,35 @@ fdescribe('ProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
   it('should be called getAll once when component started', () => {
     expect(productsServiceSpy.getAll).toHaveBeenCalledTimes(1);
   });
+
+  describe('test for getAll products', () => {
+    it('should return a list of products from service', () => {
+      //Arrange
+      const productMocks = generateManyProducts(10);
+      productsServiceSpy.getAll.and.returnValue(of(productMocks))
+      //Act
+      component.getAllProducts();
+      fixture.detectChanges();
+      //Assert
+      expect(component.products.length).toEqual(productMocks.length)
+    })
+
+    it('should render a list of products from service', () => {
+      //Arrange
+      const imgDebugElementList = fixture.debugElement.queryAll(By.css('app-product img'));
+
+      //Act
+
+      //Assert
+      imgDebugElementList.forEach((imgDebugElement,index)=>{
+        expect(imgDebugElement.attributes['src']).toEqual(productMocks[index].images[0])
+      })
+
+    })
+  })
+
 
 });
