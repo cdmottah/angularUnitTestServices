cf628777a4f468be4b684774d7d0fa0fa0cd360f
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-14 19:49:16 -0500

    test(pipes): add test cases for ReservePipe transformation logic
    Expanded `ReservePipe` test suite to include validation of transformation logic. Added test cases to confirm that the pipe reverses input strings correctly, transforming "roma" to "amor" and "123" to "321".
    
    This improves test coverage for `ReservePipe` by verifying its core functionality.

--- src/app/pipes/reserve.pipe.spec.ts
+++ src/app/pipes/reserve.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { ReservePipe } from './reserve.pipe';
 
-describe('ReservePipe', () => {
+fdescribe('ReservePipe', () => {
   it('create an instance', () => {
     const pipe = new ReservePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should transform "roma" to "amor"', ()=>{
+    const pipe= new ReservePipe();
+    const rta = pipe.transform("roma")
+    expect(rta).toEqual("amor")
+    const rta2 = pipe.transform("123")
+    expect(rta2).toEqual("321")
+  })
 });
